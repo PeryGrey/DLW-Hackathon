14c4e971f30712e22b8cfc8e830f3e4f8d515c99
PeryGrey <tdaranidarran@gmail.com>
2022-10-02 06:48:34 +0800

    nutri update

--- src/components/nutritionCounter/nutritionComponents/DisplayFoodNutrients.jsx
+++ src/components/nutritionCounter/nutritionComponents/DisplayFoodNutrients.jsx
@@ -1,58 +1,202 @@
 import React, { useState, useEffect } from 'react';
 
+// import Chart.js
+// import Chart from 'chart.js/auto';
+// import { getRelativePosition } from 'chart.js/helpers';
+
 export default function DisplayFoodNutrients() {
-  const method = 'GET';
+  const [foodData, setFoodData] = React.useState([]);
 
-  const reqURL = 'https://platform.fatsecret.com/rest/server.api';
-  const reqURLEncode =
-    'https%3A%2F%2Fplatform.fatsecret.com%2Frest%2Fserver.api ';
+  const URL = `https://trackapi.nutritionix.com/v2/search/instant?query=grilled-cheese
+  `;
 
-  // Normalized Parameters
-  const consumer_key = '9d06666db4dc474ead1642240e987944';
-  const signMethod = 'HMAC-SHA1';
-  const timeStamp = Math.round(Date.now() / 1000);
-  const nonce = Math.floor(Math.random() * 100);
-  const version = '1.0';
+  // useEffect(() => {
+  //   fetch(
+  //     'https://trackapi.nutritionix.com/v2/search/instant?query=grilled-cheese',
+  //     {
+  //       headers: {
+  //         'x-app-id': '3c79e1db',
+  //         'x-app-key': '2895bc848b17820c22877654ba3dd8a4',
+  //       },
+  //     }
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => data.common[0])
+  //     .then((value) => console.log(value));
 
-  const methodPara = 'foods.search';
-  const searchExpression = 'chicken-rice';
-  // const foodID = ;
-  const format = 'json';
-
-  const normalPara = `format%3D${format}%26method%3D${method}%26oauth_consumer_key%3D${consumer_key}%26oauth_nonce%3D${nonce}%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D${timeStamp}%26oauth_version%3D1.0`;
-
-  // const encodeMap = {
-  //   ':': '3A',
-  //   '/': '2F',
-  //   '&': '26',
-  //   '=': '3D',
-  //   '?': '3F',
-  // };
+  //   // .then((data) => console.log(data[0]));
+  // }, []);
 
   useEffect(() => {
-    fetch;
-  });
+    var myHeaders = new Headers();
+    myHeaders.append('x-app-id', '3c79e1db');
+    myHeaders.append('x-app-key', '979e5ed954ef9515c8412481074077d1');
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+    var urlencoded = new URLSearchParams();
+    urlencoded.append('query', 'apple');
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow',
+    };
+    fetch(
+      'https://trackapi.nutritionix.com/v2/natural/nutrients',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => result)
+      .then((data) =>
+        setFoodData([
+          data.foods[0]?.food_name,
+          data.foods[0].nf_calories?.toFixed(1),
+          data.foods[0].nf_total_fat?.toFixed(1),
+          data.foods[0].nf_total_carbohydrate?.toFixed(1),
+          data.foods[0].nf_protein?.toFixed(1),
+          data.foods[0].nf_saturated_fat?.toFixed(1),
+          data.foods[0].nf_cholesterol?.toFixed(1),
+          data.foods[0].nf_sodium?.toFixed(1),
+          data.foods[0].nf_dietary_fiber?.toFixed(1),
+          data.foods[0].nf_sugars?.toFixed(1),
+          data.foods[0].nf_potassium?.toFixed(1),
+          data.foods[0].nf_p?.toFixed(1),
+          data.foods[0].nf_nutrients?.toFixed(1),
+        ])
+      )
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  // pie chart
+  // const data = {
+  //   labels: ['Fat', 'Carbs', 'Protein'],
+  //   datasets: [
+  //     {
+  //       label: 'My First Dataset',
+  //       data: [foodData[2], foodData[3], foodData[4]],
+  //       backgroundColor: [
+  //         'rgb(255, 99, 132)',
+  //         'rgb(54, 162, 235)',
+  //         'rgb(255, 205, 86)',
+  //       ],
+  //       hoverOffset: 4,
+  //     },
+  //   ],
+  // };
+
+  // const ctx = document.getElementById('myChart');
+  // const chart = new Chart(ctx, {
+  //   type: 'pie',
+  //   data: data,
+  //   options: {
+  //     onClick: (e) => {
+  //       const canvasPosition = getRelativePosition(e, chart);
+
+  //       // Substitute the appropriate scale IDs
+  //       const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+  //       const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+  //     },
+  //   },
+  // });
 
   return (
     <div>
-      <div className="container foodContainer">
-        <h1 className="topic-header">Nutrients provided by </h1>
-        <div className=" food-item">Chimken Rice</div>
+      <div className="container food-container">
         <div>
           <div>
-            <p className="topic-text">Nutrition Summary</p>
-            <div className="food-table">
-              <div className="table-header">Cals</div>
-              <div className="tableEl">hello</div>
-              <div className="table-header">Fat</div>
-              <div className="tableEl">hello</div>
-              <div className="table-header">Carbs</div>
-              <div className="tableEl">hello</div>
-              <div className="table-header">Protein</div>
-              <div className="tableEl">hello</div>
+            <h1 className="topic-header">
+              Nutrients provided by {foodData[0]}
+            </h1>
+            <div className="side-by-side">
+              <div className="nutrition">
+                {/* section 1 */}
+                <h1 className="nutri-header ">Nutrition Facts</h1>
+                <p className="food-name nutri-text ">{foodData[0]}</p>
+                <p className=" nutri-text thick-bottom-border  ">
+                  Serving Size: 1
+                </p>
+                {/* section 2 */}
+                <p className="nutri-text bold">Amount per serving</p>
+                <div className=" nutri-text thin-bottom-border split-row">
+                  <p>
+                    <span className="bold">Calories</span> {foodData[1]}
+                  </p>
+                  {/* #TODO */}
+                  <p>Calories from Fat add value {}</p>
+                </div>
+                {/* section 3 */}
+                <p className="nutri-text right-align bold">% Daily Value</p>
+                <div className=" nutri-text split-row">
+                  <p>
+                    {/* #TODO */}
+                    <span className="bold">Total Fat </span> {foodData[2]}g
+                  </p>
+                  <p>get serving size weight {}%</p>
+                </div>
+                <div className="nutri-text split-row nutri-tab">
+                  <p>
+                    {/* #TODO */}
+                    Saturated Fat {foodData[5]}g
+                  </p>
+                  <p>get serving size weight {}%</p>
+                </div>
+                <div className=" nutri-text split-row">
+                  <p>
+                    {/* #TODO */}
+                    <span className="bold">Cholesterol </span> {foodData[6]}mg
+                  </p>
+                  <p>get serving size weight {}%</p>
+                </div>
+                <div className=" nutri-text split-row">
+                  <p>
+                    {/* #TODO */}
+                    <span className="bold">Sodium </span> {foodData[7]}mg
+                  </p>
+                  <p>get serving size weight {}%</p>
+                </div>
+                <div className=" nutri-text split-row">
+                  <p>
+                    {/* #TODO */}
+                    <span className="bold">Potassium </span> {foodData[10]}mg
+                  </p>
+                  <p>get serving size weight {}%</p>
+                </div>
+                <div className=" nutri-text split-row">
+                  <p>
+                    {/* #TODO */}
+                    <span className="bold">Total Carbohydrates </span>{' '}
+                    {foodData[2]}g
+                  </p>
+                  <p>get serving size weight {}%</p>
+                </div>
+                <div className="nutri-text split-row nutri-tab">
+                  <p>
+                    {/* #TODO */}
+                    Dietary Fiber {foodData[8]}g
+                  </p>
+                  <p>get serving size weight {}%</p>
+                </div>
+                <div className="nutri-text nutri-tab">
+                  <p>
+                    {/* #TODO */}
+                    Sugars {foodData[9]}g
+                  </p>
+                </div>
+                <div className=" nutri-text">
+                  <p>
+                    {/* #TODO */}
+                    <span className="bold">Protein </span> {foodData[4]}g
+                  </p>
+                </div>
+                <p className="foot-note">
+                  * Percent Daily Values are based on a 2000 calorie diet.
+                </p>
+              </div>
+
+              <div>
+                <canvas id="myChart" width="400" height="400"></canvas>
+              </div>
             </div>
           </div>
-          <div className="nutrition">Nutrition Facts</div>
         </div>
       </div>
     </div>
