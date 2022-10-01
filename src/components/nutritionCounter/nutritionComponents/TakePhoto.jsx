c10cf03aeea628d35c2eeb2ad8f5d20fa49ad25a
Ian Chan Jia Jun <72877650+normalwalrus@users.noreply.github.com>
2022-10-01 21:07:05 +0800

    Webcam code

--- src/components/nutritionCounter/nutritionComponents/TakePhoto.jsx
+++ src/components/nutritionCounter/nutritionComponents/TakePhoto.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
+import Webcam from "react-webcam";
 
 // camera imports
 import Camera from 'react-html5-camera-photo';
@@ -9,6 +10,14 @@ import 'react-html5-camera-photo/build/css/index.css';
 
 export default function NutritionCounter() {
   const [dataUri, setDataUri] = useState('');
+
+  const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = React.useState(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgSrc(imageSrc);
+  }, [webcamRef, setImgSrc]);
 
   function handleTakePhotoAnimationDone(dataUri) {
     console.log('takePhoto');
@@ -20,6 +29,17 @@ export default function NutritionCounter() {
   return (
     <div className="container take-picture">
       <h1 className="topic-header">Let's take a picture of your food!</h1>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <button onClick={capture}>Capture photo</button>
+      {imgSrc && (
+        <img
+          src={imgSrc}
+        />
+      )}
     </div>
   );
 }
